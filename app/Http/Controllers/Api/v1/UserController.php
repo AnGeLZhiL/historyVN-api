6fdd000e6d72f6dd742aadbac76c8df78f130bb6
AnGeLZhiL <angelina.zhilyakova.2002@mail.ru>
2023-04-12 23:52:34 +0300

    Сделала пробную валидацию данных при регистрации

---
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterUserRequest;
+
+class UserController extends Controller
+{
+    public function register(RegisterUserRequest $request){
+
+    }
+}
